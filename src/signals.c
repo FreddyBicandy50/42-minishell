3a6c46d6f6abba796afda34707d2fa45af91260a
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-13 14:29:50 +0300

    upload

--- src/signals.c
+++ src/signals.c
@@ -6,11 +6,7 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:49:04 by amokdad           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/10/11 10:25:07 by amokdad          ###   ########.fr       */
-=======
-/*   Updated: 2024/10/12 16:10:46 by fbicandy         ###   ########.fr       */
->>>>>>> refs/remotes/origin/main
+/*   Updated: 2024/10/13 14:29:37 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +27,6 @@ void	handle_sigint(__attribute__((unused)) int sig)
 	rl_redisplay();
 }
 
-<<<<<<< HEAD
-// Handle (Ctrl+c)
-// Handle (Ctrl+\) //do nothing
-=======
-void	handle_sigquit(__attribute__((unused)) int sig)
-{
-	signal(SIGQUIT, SIG_IGN);
-}
-
->>>>>>> refs/remotes/origin/main
 void	signals(void)
 {
 	signal(SIGINT, handle_sigint);
