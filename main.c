1efb1de7a9cc9b6cd7d4856650b9a64b2dbb8ffe
ali-al-mokdad <amokdad058@gmail.com>
2024-10-15 17:21:47 +0300

    add envp + continu check from path

--- main.c
+++ main.c
@@ -3,20 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amokdad <amokdad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 16:51:28 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/10/12 20:12:23 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:19:36 by amokdad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "src/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
 	char	*prompt;
 	t_data	data;
 
+	(void)argc;
+	(void)argv;
+	(void)envp;
 	prompt = "\001\e[45m\002>>> \001\e[0m\e[33m\002 Minishell>$ \001\e[0m\002";
 	signals();
 	while (1)
--- src/check_cmd.c
+++ src/check_cmd.c
@@ -3,16 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   check_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amokdad <amokdad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 13:37:16 by amokdad           #+#    #+#             */
-/*   Updated: 2024/10/15 00:47:02 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:17:34 by amokdad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	check_cmd_if_included(t_cmd *cmd)
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (0);
+}
+
+void	check_cmd_in_envp(t_cmd *cmd, char *envp[])
+{
+	char	*path;
+	int		i;
+
+	path = find_path(cmd->command, envp);
+	i = -1;
+	if (!path)
+	{
+		perror("Error");
+	}
+}
+
+//check cmd if included in our code or include in sys 
+void	check_cmd_if_included(t_cmd *cmd, char *envp[])
 {
 	if (ft_strcmp(cmd->command, "echo") == 0)
 		printf("\necho //\n");
@@ -29,5 +72,5 @@ void	check_cmd_if_included(t_cmd *cmd)
 	else if (ft_strcmp(cmd->command, "exit") == 0)
 		printf("\nexit\n");
 	else
-		printf("\ngo to pipes");
+		check_cmd_in_envp(cmd, envp);
 }
